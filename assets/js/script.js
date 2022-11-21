49c6252a4aade4b667cd30c2c839aaf2a8791c64
bmfoster5 <foster.brandon96@gmail.com>
2022-11-21 08:32:04 -0500

    script push

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var seachForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history'); 
+
+var searchHistory = [];
+var weatherRoot = 'https://api.openweathermap.org';
+var apiKey = '816f5eec2918e5335373901c86bd48da';
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+function renderSearchHistory() {
+    searchHistoryContainer.innerHTML = " ";
+
+    for (var i = searchHistory.length -1; i>= 0. i--) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
+    }
+}
+
+
